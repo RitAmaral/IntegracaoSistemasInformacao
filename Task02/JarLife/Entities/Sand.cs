aa37c2b4ddb2a45b87c36fa833b0aa18bf963f4a
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-06 16:47:44 +0100

    Add files via upload

---
+++ Task02/JarLife/Entities/Sand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JarLife.Entities
+{
+    internal class Sand : Jar //golf balls herda da classe Jar
+    {
+        //Propriedades
+        public string Procrastinate { get; set; }
+        public string Quarrels { get; set; }
+
+        //Construtores
+        public Sand() { }
+        public Sand(string procrastinate, string quarrels)
+        {
+            Procrastinate = procrastinate;
+            Quarrels = quarrels;
+        }
+        public Sand(string name, DateOnly birthDate, string nationality, string gender, string procrastinate, string quarrels) :base(name, birthDate, nationality, gender)
+        {
+            Procrastinate = procrastinate;
+            Quarrels = quarrels;
+        }
+        //Métodos
+        public override void Detalhes()
+        {
+            Console.WriteLine($"Procrastinar? O {Name}? {Procrastinate} .. mas não ocupa muito do seu tempo com isso. Perder tempo com brigas sem importância? {Quarrels}.");
+        }
+    }
+}
