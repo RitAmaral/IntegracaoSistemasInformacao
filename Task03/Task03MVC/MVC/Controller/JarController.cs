61eace5173a5fd14ea9c9066119f347c043060b8
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-07 16:38:00 +0100

    Add files via upload

---
+++ Task03/Task03MVC/MVC/Controller/JarController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task03MVC.MVC.Model;
+using Task03MVC.MVC.View;
+
+namespace Task03MVC.MVC.Controller
+{
+    internal class JarController //Controller/Controlador
+    {
+        //Propriedades
+        private JarModel _model;
+        private JarView _view;
+        //Construtor
+        public JarController(JarModel model, JarView view)
+        {
+            _model = model;
+            _view = view;
+        }
+        //Métodos
+        public void MostrarDetalhes(int details)
+        {
+            JarModel jar = _model.ObterDetalhes(details);
+            _view.EscreverDetalhesJar(jar.Name, jar.BirthDate, jar.Nationality, jar.Gender);
+        }
+    }
+}
---
+++ Task03/Task03MVC/MVC/View/JarView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task03MVC.MVC.Model;
+using Task03MVC.MVC.Controller;
+
+namespace Task03MVC.MVC.View
+{
+    internal class JarView //View/Visualizador
+    {
+        //Método
+        public void EscreverDetalhesJar(string name, DateOnly birthDate, string nationality, string gender)
+        {
+            Console.WriteLine("Detalhes do Jar: ");
+            Console.WriteLine("Nome: " + name);
+            Console.WriteLine("Data de Nascimento: " +  birthDate);
+            Console.WriteLine("Nacionalidade: " + nationality);
+            Console.WriteLine("Género: " + gender);
+        }
+    }
+}
