c1929f3fc934b3744d2da305e521762678cca521
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-07 19:55:32 +0100

    Add files via upload

---
+++ Task03/Task03MVC/MVC/Model/MedicoModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task03MVC.MVC.View;
+using Task03MVC.MVC.Controller;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Task03MVC.MVC.Model
+{
+    internal class MedicoModel //Model/Modelo
+    {
+        //Propriedades
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        public string Localidade { get; set; }
+        public DateOnly DataNasc { get; set; }
+        public bool CartaConducao { get; set; }
+        public double Vencimento { get; set; }
+        public int CC { get; set; }
+        public int NIF { get; set; }
+        public int Telemovel { get; set; }
+        public string Email { get; set; }
+        public string NomeUtil { get; set; }
+        //private string _password { get; set; }
+        public string Password { get; set; }
+
+        //Método
+        public MedicoModel ObterDetalhes(int details)
+        {
+            return new MedicoModel
+            {
+                ID = 22,
+                Nome = "Alex",
+                Genero = "Masculino",
+                Localidade = "Braga",
+                DataNasc = DateOnly.Parse("01/01/2000"),
+                CartaConducao = true,
+                Vencimento = 2000,
+                CC = 12815633,
+                NIF = 211792762,
+                Telemovel = 916124429,
+                Email = "carlos@gmail.com",
+                NomeUtil = "Med22",
+                //_password = "******"
+                Password = "******"
+            };
+        }
+
+    }
+}
---
+++ Task03/Task03MVC/MVC/View/MedicoView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task03MVC.MVC.Model;
+using Task03MVC.MVC.Controller;
+using System.Reflection;
+using System.Xml.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Task03MVC.MVC.View
+{
+    internal class MedicoView //View/Visualizador
+    {
+        //Método
+        public void EscreverDetalhesMedico(int id, string nome, string genero, string localidade, DateOnly dataNasc, bool cartaConducao, double vencimento, int cc, int nif, int telemovel, string email, string nomeUtil, string password)
+        {
+            Console.WriteLine("Detalhes do Médico: ");
+            Console.WriteLine();
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Género: " + genero);
+            Console.WriteLine("Localidade: " + localidade);
+            Console.WriteLine("Data de Nascimento: " +  dataNasc.ToString("dd/MM/yyyy"));
+            Console.WriteLine("Carta de Condução: " + cartaConducao);
+            Console.WriteLine("Vencimento: " + vencimento);
+            Console.WriteLine("CC: " + cc);
+            Console.WriteLine("NIF: " + nif);
+            Console.WriteLine("Telemóvel: " + telemovel);
+            Console.WriteLine("Email: " + email);
+            Console.WriteLine("Nome de Utilizador: " + nomeUtil);
+            Console.WriteLine("Password: " + password);
+        }
+        public void MensagemLoginAceite(string nome)
+        {
+            Console.WriteLine($"Login Aceite! Bem-vindo {nome}!");
+        }
+        public void MensagemLoginErrado(string nome)
+        {
+            Console.WriteLine($"Login Errado! Tenta novamente, {nome}!");
+        }
+        public void MensagemLogout(string nome)
+        {
+            Console.WriteLine($"Até breve {nome}! \n--------------- \nSessão terminada.");
+        }
+
+    }
+}
