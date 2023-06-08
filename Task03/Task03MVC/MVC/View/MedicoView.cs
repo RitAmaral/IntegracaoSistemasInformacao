fff139e3373ab1d138e4244ab7d785bf06f6a4f8
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-08 15:24:44 +0100

    Add files via upload

---
+++ Task03/Task03MVC/MVC/View/MedicoView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task03MVC.MVC.Model;
+using Task03MVC.MVC.Controller;
+using System.Reflection;
+using System.Xml.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Task03MVC.MVC.View
+{
+    internal class MedicoView //View/Visualizador
+    {
+        public void EscreverDetalhesMedico(string funcao, int id, string nome, string genero, string localidade, DateOnly dataNasc, bool cartaConducao, double vencimento, int cc, int nif, int telemovel, string email, string nomeUtil, string password)
+        {
+            Console.WriteLine("Detalhes: ");
+            Console.WriteLine();
+            Console.WriteLine("Função: " + funcao);
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Género: " + genero);
+            Console.WriteLine("Localidade: " + localidade);
+            Console.WriteLine("Data de Nascimento: " +  dataNasc.ToString("dd/MM/yyyy"));
+            Console.WriteLine("Carta de Condução: " + cartaConducao);
+            Console.WriteLine("Vencimento: " + vencimento);
+            Console.WriteLine("CC: " + cc);
+            Console.WriteLine("NIF: " + nif);
+            Console.WriteLine("Telemóvel: " + telemovel);
+            Console.WriteLine("Email: " + email);
+            Console.WriteLine("Nome de Utilizador: " + nomeUtil);
+            Console.WriteLine("Password: " + password);
+        }
+        public void MensagemLoginAceite(string nome)
+        {
+            Console.WriteLine($"Login Aceite! Bem-vindo {nome}!"); //mensagem que vai aparecer se o login for aceite.
+            Console.WriteLine();
+        }
+        public void MensagemLoginErrado(string nome)
+        {
+            Console.WriteLine($"Login Errado! Tenta novamente, {nome}!"); //mensagem que vai aparecer se o login for errado.
+        }
+        public void MensagemLogout(string nome)
+        {
+            Console.WriteLine($"Até breve {nome}! \n--------------- \nSessão terminada."); //mensagem que vai ser feita quando fizer logout.
+        }
+
+    }
+}
---
+++ Task03/Task03MVC/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Task03MVC.MVC.Controller;
+using Task03MVC.MVC.Model;
+using Task03MVC.MVC.View;
+
+namespace Task03MVC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*------------------------------------Padrão de Arquitetura MVC----------------------------*/
+
+            /* 1º criar 3 pastas, uma para controller, outra para model e outra para view. Depois criar, em cada pasta criada, uma classe
+            model, view e controller, respetivamente.
+            */
+            //---------------------------------------Colaborador------------------------------------
+
+            //Criar model
+            ColaboradorModel colabmodel = new ColaboradorModel();
+            //Criar view
+            ColaboradorView colabview = new ColaboradorView();
+            //Criar controller
+            ColaboradorController colabcontroller = new ColaboradorController(colabmodel, colabview);
+
+            //Exibição dos detalhes do colaborador adicionado através do método criado no ColaboradorModel (ObterDetalhesColab)
+            colabcontroller.MostrarDetalhesColab(1);
+            Console.WriteLine();
+
+            //Adicionar colaboradores à lista de colaboradores
+            colabcontroller.AddColaborador("Motorista", 20, "Tomás Rodrigues", "Masculino", "Braga", DateOnly.Parse("12/05/1980"), true, 1000, 13415625, 231562784, 966126621, "tomas@gmail.com");
+            colabcontroller.AddColaborador("Enfermeira", 21, "Maria Santos", "Feminino", "Braga", DateOnly.Parse("22/08/1987"), false, 1500, 18423790, 225432790, 936125512, "maria@gmail.com");
+            
+            //Exibição dos detalhes dos colaboradores da lista, através do método criado no ColaboradorController
+            colabcontroller.MostrarColaborador(0);
+            Console.WriteLine();
+            colabcontroller.MostrarColaborador(1);
+            Console.WriteLine();
+
+            //Se quiser remover alguém da lista de colaboradores:
+            //colabcontroller.RemoveColaborador(1); 
+            //Console.ReadLine();
+            
+
+            //---------------------------------------Médico------------------------------------
+
+            //Criar model
+            MedicoModel medmodel = new MedicoModel();
+            //Criar view
+            MedicoView medview = new MedicoView();
+            //Criar controller
+            MedicoController med22 = new MedicoController(medmodel, medview);
+
+            //Exibição dos detalhes do Médico através do método criado no MedicoController
+            med22.MostrarDetalhes(1); //inclui o nome de utilizador e password para poder fazer o login em baixo.
+            Console.WriteLine();
+
+            //Exibição das mensagens de login e logout:
+            if (med22.Login(1) == true)
+            {
+                //med22.SetPassword(1); //isto está comentado porque não está a funcionar / a password não está a ser alterada.
+                //med22.Login(1); //isto foi usado para tentar colocar a nova password, no entanto diz sempre login errado, por isso o método SetPassword não está a funcionar.
+                med22.Logout(1);
+            }
+
+        }
+    }
+}
