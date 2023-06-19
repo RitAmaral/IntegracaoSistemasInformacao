eea9652bef8d61d335ff535ca0222af27b6a645a
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-19 19:52:03 +0100

    anotacoes v02

---
+++ Task04/Anotacoes_v02/Anotacao_DAL/AnotacoesAula_DAO.cs
@@ -0,0 +1,157 @@
+﻿using Anotacao_BO;
+using System.Xml.Serialization;
+using Anotacoes_Constantes;
+using SerializeTools;
+using static Anotacao_BO.RegistoAnotacao;
+using ToolBox;
+
+namespace Anotacao_DAL
+{
+    public class AnotacoesAula_DAO
+    {
+        [XmlRoot(ElementName = "Anotacoes")]
+        public class AnotacoesBD //criar classe, vai representar a base de dados; é composta por uma lista de anotações
+        {
+            public AnotacoesBD()
+            {
+                Items = new List<RegistoAnotacao>();
+            }
+
+            [XmlElement(ElementName = "Anotacao")]
+            public List<RegistoAnotacao> Items { get; set; }
+        }
+
+        //Criação da lista
+        //private List<AnotacoesAula> _anotacoesList;
+        private AnotacoesBD _anotacoesList;
+        private DateTime _loaded; //variaveis adicionadas (_loaded e _modified) para o momento em que carreguei, e quando foi modificada
+        private DateTime _modified;
+
+        public AnotacoesAula_DAO()
+        {
+            _anotacoesList = new AnotacoesBD();
+        }
+
+        //Métodos
+        public bool AdicionarAnotacao(AnotacoesAula anotacao)
+        {
+            if (ReferenceEquals(anotacao, null)) return false; //se enviar objeto nulo não vale a pena tar a perder tempo, logo return false
+            return AdicionarAnotacao(anotacao.RegistoAnotacao());
+        }
+        public bool AdicionarAnotacao(RegistoAnotacao anotacao)
+        { // um struct está sempre preenchido
+            _anotacoesList.Items.Add(anotacao); //adiciona à lista
+            _modified = DateTime.Now;
+            return true;
+        }
+        public List<string> GetAnotacoesList() 
+        {
+            List<string> list = new List<string>();
+            foreach (RegistoAnotacao c in _anotacoesList.Items)
+            {
+                list.Add(c.ToString());
+            }
+            return list;
+        }
+        public bool ApagarAnotacao(string nome) //apagar pelo nome
+        {
+            AnotacoesAula? obj = null;
+            string tNome = nome.Trim();
+            if (ExisteAnotacao(tNome, out obj))
+            {
+                if (ReferenceEquals(obj, null)) return false;
+                // apagar todos os registos com o nome igual ao "nome"
+                if (_anotacoesList.Items.RemoveAll(r => r.Nome.Equals(nome)) > 0) //se for maior que 0, significa que eliminou pelo menos 1 registo.
+                {
+                    _modified = DateTime.Now;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool ExisteAnotacao(string nome)
+        {
+            AnotacoesAula? obj = null;
+            return ExisteAnotacao(nome, out obj);
+        }
+
+        public bool ExisteAnotacao(string nome, out AnotacoesAula? obj)
+        {
+            obj = null;
+            string tNome = nome.Trim();
+            if (tNome.Length == 0) return false;
+            int tIndex = _anotacoesList.Items.FindIndex(r => r.Nome.Equals(nome));
+            if (tIndex > -1)
+            {
+                obj = new AnotacoesAula(_anotacoesList.Items[tIndex]);
+                return true;
+            }
+            return false;
+        }
+        public void ExportarDados()
+        {
+            if (_modified > _loaded || !File.Exists(Constantes.NomeXmlAnotacoes))
+            {
+                try
+                {
+                    ExportarXml(Constantes.NomeXmlAnotacoes);
+                    _modified = _loaded = DateTime.Now;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+        public bool ExportarXml(string ficheiro)
+        {
+            if (ficheiro != null)
+            {
+                try
+                {
+                    XmlMethods.SerializeToXml<AnotacoesBD>(_anotacoesList, ficheiro);
+
+                    return true;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return false;
+        }
+        public bool ImportarDados()
+        {
+            return ImportarXml(Constantes.NomeXmlAnotacoes);
+        }
+        public bool ImportarXml(string ficheiro)
+        {
+            if (ficheiro != null && File.Exists(ficheiro))
+            {
+                try
+                {
+                    _anotacoesList = XmlMethods.DeserializeXmlToObject<AnotacoesBD>(ficheiro);
+                    _loaded = DateTime.Now;
+                    _modified = DateTime.Now;
+                    // uma proposta de solução para evitar duplicação de Id's
+                    // estratégia para se atualizar o gerador de Id's
+                    if (_anotacoesList.Items.Count > 0)
+                    {
+                        int tId = _anotacoesList.Items[0].Id;
+                        foreach (RegistoAnotacao r in _anotacoesList.Items)
+                        {
+                            if (r.Id > tId) tId = r.Id;
+                        }
+                        GetNewId.Instancia.ResetProximo(tId);
+                    }
+                    return true;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return false;
+        }
+    }
+}
