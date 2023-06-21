bfae3a078e315680afccd3bc1ce57fdbeb25f6df
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-21 21:35:04 +0100

    adição pais e alterações

--- ProjetoFinal_Rita/Eurovisao_Console/Program.cs
+++ ProjetoFinal_Rita/Eurovisao_Console/Program.cs
@@ -9,14 +9,23 @@ namespace Eurovisao_Console
         {
             Eurovisao_BR gestaoEurovisao = new Eurovisao_BR();
 
-            Console.WriteLine("ID\t|Pais\t|Nome Representante\t|Nome Musica\t|Pontos Juri\t|Pontos Televoto\t|Total Pontos\t|Classificação Final");
+            Console.WriteLine("ID\t|Pais\t|Representante\t|Musica\t|Juri\t|Televoto\t|Total Pontos\t|Classificação Final");
 
-            Eurovisao pais1 = gestaoEurovisao.NovoConcorrente("Portugal", "Mimicat", "Ai Coração", 43, 16, 23);
-            gestaoEurovisao.AdicionarConcorrente(pais1);
+            //Adicionar concorrentes:
+            Eurovisao conc1 = gestaoEurovisao.NovoConcorrente("Portugal", "Mimicat", "Ai Coração", 43, 16, 23);
+            Eurovisao conc2 = gestaoEurovisao.NovoConcorrente("Suécia", "Loreen", "Tattoo", 340, 243, 1);
 
+            //Adicionar os concorrentes criados em cima à lista:
+            gestaoEurovisao.AdicionarConcorrente(conc1);
+            gestaoEurovisao.AdicionarConcorrente(conc2);
+
+            //Mostra lista de concorrentes da Eurovisão:
             MostrarLista(gestaoEurovisao.GetConcorrentesList());
+            Console.WriteLine();
+
+            //Se quiser apagar concorrentes da lista:
             if (gestaoEurovisao.ApagarConcorrente("Portugal"))
-                Console.WriteLine("ID\t|Pais\t|Nome Representante\t|Nome Musica\t|Pontos Juri\t|Pontos Televoto\t|Total Pontos\t|Classificação Final");
+                Console.WriteLine("ID\t|Pais\t|Representante\t|Musica\t|Juri\t|Televoto\t|Total Pontos\t|Classificação Final");
                 MostrarLista(gestaoEurovisao.GetConcorrentesList());
         }
         private static void MostrarLista(List<string> lista)
