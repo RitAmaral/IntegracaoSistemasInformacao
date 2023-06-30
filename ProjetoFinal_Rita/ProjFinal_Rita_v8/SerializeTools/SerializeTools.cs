d828c4f0fb335be8da1753c8226695377494618e
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-06-30 11:04:23 +0100

    proj v8 com net 7.0 e force clean

--- ProjetoFinal_Rita/ProjFinal_Rita_v8/SerializeTools/SerializeTools.cs
+++ ProjetoFinal_Rita/ProjFinal_Rita_v8/SerializeTools/SerializeTools.cs
@@ -1,50 +1,83 @@
-﻿using System.IO;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 namespace SerializeTools
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class XmlMethods
     {
-        public static string SerializeToXml<T>(T anyobject) where T : class
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="anyobject"></param>
+        /// <returns></returns>
+        public static string SerializeToXml<T>(T anyobject) where T : class  
         {
-            XmlSerializer xmlSerializer = new XmlSerializer(anyobject.GetType());
-
-            using (TextWriter writer = new StringWriter())
+            if (!ReferenceEquals(anyobject, null))
             {
-                xmlSerializer.Serialize(writer, anyobject);
-                return writer.ToString();
-            }
-        }
+                XmlSerializer xmlSerializer = new XmlSerializer(anyobject.GetType());
 
-        public static void SerializeToXml<T>(T anyobject, string xmlFilePath) where T : class
-        {
-            XmlSerializer xmlSerializer = new XmlSerializer(anyobject.GetType());
-
-            using (StreamWriter writer = new StreamWriter(xmlFilePath))
+                using (TextWriter writer = new StringWriter())
+                {
+                    xmlSerializer.Serialize(writer, anyobject);
+                    return writer.ToString() ?? "";
+                }
+            } 
+            else
             {
-                xmlSerializer.Serialize(writer, anyobject);
+                return "";
             }
-        }
+        }  
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="anyobject"></param>
+        /// <param name="xmlFilePath"></param>
+        public static void SerializeToXml<T>(T anyobject, string xmlFilePath) where T : class  
+        {  
+            if (!ReferenceEquals(anyobject, null))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(anyobject.GetType());
 
-        public static T DeserializeXmlStringToObject<T>(string xml) where T : class
-        {
+                using (StreamWriter writer = new StreamWriter(xmlFilePath))
+                {
+                    xmlSerializer.Serialize(writer, anyobject);
+                }
+            }
+        }  
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xml"></param>
+        /// <returns></returns>
+        public static T DeserializeXmlStringToObject<T>(string xml) where T : class  
+        {  
             System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T));
             var stream = new MemoryStream();
             var writer = new StreamWriter(stream);
             writer.Write(xml);
             writer.Flush();
             stream.Position = 0;
-            return (T)ser.Deserialize(stream);
-        }
-
-        public static T DeserializeXmlToObject<T>(string filepath) where T : class
-        {
-            System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T));
-
-            using (StreamReader sr = new StreamReader(filepath))
-            {
-                return (T)ser.Deserialize(sr);
-            }
-        }
+            return (T)ser.Deserialize(stream)!;
+        }  
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="filepath"></param>
+        /// <returns></returns>
+        public static T DeserializeXmlToObject<T>(string filepath) where T : class  
+        {  
+            System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T));  
+  
+            using (StreamReader sr = new StreamReader(filepath))  
+            {  
+                return (T)ser.Deserialize(sr)!;  
+            }  
+        }  
     }
 }
