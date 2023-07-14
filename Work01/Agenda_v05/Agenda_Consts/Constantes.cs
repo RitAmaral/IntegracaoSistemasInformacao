2a62c470a8851f49a3d76a8a39c3e164919d0a70
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-07-14 12:58:04 +0100

    agendav5

---
+++ Work01/Agenda_v05/Agenda_Consts/Constantes.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Agenda_Consts
+{
+    public class Constantes
+    {
+        public const string NomeXmlCompromissos = "ListaCompromissos.xml";
+
+        public const string NomeXmlConfiguration = "AgendaCFG.xml"; //constante necessária para o projeto “Agenda_Configuration”
+    }
+}
